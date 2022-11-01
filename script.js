d034a9a908ff9c99c6806c45c670858787dfffea
Jasper Oudemans <jasperoudemans@gmail.com>
2022-11-01 08:19:07 -0400

    fixed typo

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ blessingBtn.addEventListener("click", function () {
   const namePlace = document.getElementById("nameRes");
   namePlace.textContent =
     name +
-    ", I am sending my pure Love to you through my gaze in the photograph below. Look into my eyes and bask in the feeling of complete Love and understanding, for I understands your truest nature as Love. Remain in my gaze for as long as you wish, then go about your day feeling blessed";
+    ", I am sending my pure Love to you through my gaze in the photograph below. Look into my eyes and bask in the feeling of complete Love and understanding, for I understand your truest nature as Love. Remain in my gaze for as long as you wish, then go about your day feeling blessed";
   const blessDiv = document.getElementById("blessing");
   console.log(blessDiv);
   console.log(blessDiv.className);
